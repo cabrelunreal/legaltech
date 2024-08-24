6c0e29c13e3c2a6b382f0c14d1123dd28f75022b
lunaking1 <cabreltchoffo12@gmail.com>
2024-08-24 14:40:10 +0100

    deployement session 24th

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 
 import Welcome from "./pages/Home/page";
-import Register from "./pages/Register/page";
-import TranslateSuccess from "./pages/successTraduction/success";
 
 export default function Home() {
   return (
     <div>
-      <Home />
+      <Welcome />
     </div>
   );
 }

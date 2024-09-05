b343ffb39b26dcb9e8de9809d93473d227fcde56
lunaking1 <cabreltchoffo12@gmail.com>
2024-09-05 15:12:18 +0100

    deploy font-family

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
+      
       <body>{children}</body>
     </html>
   );

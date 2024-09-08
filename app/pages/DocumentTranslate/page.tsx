25ae3f363ba502b0248a9a1e84a58788590bb2fd
lunaking1 <cabreltchoffo12@gmail.com>
2024-09-08 13:17:55 +0100

    remove why us

--- app/pages/DocumentTranslate/page.tsx
+++ app/pages/DocumentTranslate/page.tsx
@@ -68,7 +68,7 @@ const DocTranslates = () => {
                 title='TRADUCTION DE DOCUMENTS'
                 image='https://res.cloudinary.com/dedakscgk/image/upload/v1719703638/translate-documents_grl9qr.png'
                 description='Traduction de documents juridiques, administratifs et autres documents techniques.'
-                custbutton='bg-green-700 text-white px-3 py-2 rounded-md uppercase hover:bg-green-800'
+                custbutton='bg-green-700 text-white px-3 py-2 md:py-3 rounded-md uppercase hover:bg-green-800'
                 buttonTitle1='commencer'
                 custom='text-center'
             />
